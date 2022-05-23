62c0ad1457a447f6e5395151b7c65476d4071f87
barbara-tshimologong <barbara@tshimologong.joburg>
2022-05-23 15:41:21 +0200

    Add project files.

---
+++ AFCentral.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AFCentral.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter your FirstName");
+            var firstName = System.Console.ReadLine();
+            System.Console.WriteLine("Please enter your SurName");
+            var surname = System.Console.ReadLine();
+            System.Console.WriteLine("Please enter your Printer code");
+            var printerCode = System.Console.ReadLine();
+
+            DataAccess.SqlServerAccess ssa = new DataAccess.SqlServerAccess(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AFCentral;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            System.Console.WriteLine(ssa.CallSP());
+
+
+        }
+    }
+}
---
+++ DataAccess/SqlServerAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class SqlServerAccess
+    {
+
+        public SqlServerAccess(string connString)
+        {
+            ConnString = connString;
+        }
+
+         public string ConnString { get; init; }
+
+        public string CallSP()
+        {
+            using (SqlConnection cn = new Microsoft.Data.SqlClient.SqlConnection())
+            {
+                cn.ConnectionString = ConnString;
+
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cn;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.CommandText = "spInsert_Facilitator";
+
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            return "Successfully inserted";
+        }
+    }
+}
